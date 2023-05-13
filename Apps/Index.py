dbc32fe8a854254c8c976019c3e42677b8f5303c
tangge233 <2697992791@qq.com>
2023-05-13 16:42:28 +0800

    fix: 文件目录 Bug

--- Apps/Index.py
+++ Apps/Index.py
@@ -3,12 +3,13 @@ import json
 
 json_data = []
 
+
 print("Start preparing the Index file")
 
-for filename in os.listdir():
+for filename in os.listdir("Apps"):
     #print("Current file:",filename)
     if filename.endswith('.json') and filename != 'Index.json':
-        with open(filename,"r",encoding="utf-8") as AppInfo:
+        with open("Apps/" + filename,"r",encoding="utf-8") as AppInfo:
             data = json.load(AppInfo)
             ThisAppInfo={
                 "Name" : data['Name'],
@@ -24,7 +25,7 @@ for filename in os.listdir():
             json_data.append(ThisAppInfo)
 
 # 将所有数据写入 result.json 文件中
-with open('Index.json', 'w',encoding="utf-8") as f:
+with open('Apps/Index.json', 'w',encoding="utf-8") as f:
     json.dump(json_data, f,ensure_ascii = False)
 
 print("Index file preparation completed")
