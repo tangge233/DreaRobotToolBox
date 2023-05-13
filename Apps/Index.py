40206cdf9713070f22a3f87d568fa878ada2a030
tangge233 <2697992791@qq.com>
2023-05-13 14:02:59 +0800

    应用快捷下载初始化

---
+++ Apps/Index.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+json_data = []
+
+print("开始整理应用信息……")
+
+for filename in os.listdir():
+    #print("Current file:",filename)
+    if filename.endswith('.json') and filename != 'Index.json':
+        with open(filename,"r",encoding="utf-8") as AppInfo:
+            data = json.load(AppInfo)
+            ThisAppInfo={
+                "Name" : data['Name'],
+                "Version" : data['Version'],
+                "Author" : {
+                    "Name" : data['Author']['Name'],
+                    "HomePage" : data['Author']['HomePage']
+                },
+                "HomePage" : data['HomePage']
+            }
+            #print(ThisAppInfo)
+            json_data.append(ThisAppInfo)
+
+# 将所有数据写入 result.json 文件中
+with open('Index.json', 'w',encoding="utf-8") as f:
+    json.dump(json_data, f,ensure_ascii = False)
+
+print("操作结束")
